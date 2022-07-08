4c3407254fa83dba8cf5ba2915e4ecf0ee538701
Anna Kovalik <annhuddy13@gmail.com>
2022-07-08 18:19:07 +0300

    Feat: add init files

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin =require( "html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports={
+mode: 'development',
+    entry: ["@babel/polyfill",'./src/index.jsx'],
+    output:{
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[hash].js',
+    },
+    resolve: {
+        extensions: [".js", ".jsx"],
+    },
+    devServer: {
+        port: 3000,
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin()
+    ],
+    module:{
+    rules:[
+        {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        },{
+            test: /\.m?jsx$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ['@babel/preset-env', "@babel/preset-react"]
+                }
+            }
+        },
+        {
+            test: /\.(css|scss)$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg)$/,
+            use: ['file-loader']
+        }
+    ]
+    }
+
+
+}
